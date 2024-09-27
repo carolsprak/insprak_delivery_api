ca7c28618d9ec50e5e569a0762b57b9c5cf67da8
Anne Caroline Rocha <carolsprak@gmail.com>
2024-09-27 00:20:17 -0300

    update server.js

--- release.md
+++ release.md
@@ -1,5 +1,3 @@
 ## Insprak Delivery
 
-1. Cadastro de usuário
-2. Autenticação de usuário 
-3. Identificação de nome de usuário
+1. Autenticação de usuário 
--- server.js
+++ server.js
@@ -1,29 +1,27 @@
 const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('./database/db.json')
-const middlewares = jsonServer.defaults()
-const bodyParser = require('body-parser')
+const middlewares = jsonServer.defaults() 
 
-const PORT = process.env.PORT || 4589
+const PORT = process.env.PORT || 4501
 
-server.use(middlewares)
-server.use(bodyParser.json())
+server.use(middlewares)  
 
 server.post('/login', (req, res) => {
   const users = router.db.get('users').value()
-  const { username, password } = req.body
+  const { username, password } =  req.body
 
   const user = users.find(u => u.username === username && u.password === password)
 
   if (user) {
     res.json({ success: true, userId: user.id })
   } else {
-    res.status(401).json({ success: false, message: 'Usuário aqou senha incorretos.' })
-  } 
+    res.status(401).json({ success: false, message: 'Usuário ou senha incorretos.' })
+  }
 })
 
-
 server.use(router)
+
 
 server.listen(PORT, () => {
   console.log(`JSON Server is running on port ${PORT}`)
